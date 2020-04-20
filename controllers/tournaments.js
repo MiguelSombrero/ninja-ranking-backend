8ef91725d72631fcca8241697874cfbf48562d56
Miika Somero <miika.somero@gmail.com>
2020-04-20 15:51:42 +0300

    added update router for tournament

--- controllers/tournaments.js
+++ controllers/tournaments.js
@@ -1,20 +1,33 @@
 const { executeQuery } = require('../services/dbService')
 const middleware = require('../utils/middleware')
 const tournamentsRouter = require('express').Router()
+const { SELECT_TOURNAMENTS, INSERT_TOURNAMENT, UPDATE_TOURNAMENT } = require('../db/queries')
 
 tournamentsRouter.get('/', async (req, res, next) => {
-  const rows = await executeQuery(
-    'SELECT Tournament.id, Tournament.account_id, Tournament.name, Tournament.created, Tournament.active, json_agg(Obstacle) AS obstacles FROM Tournament LEFT JOIN Obstacle ON Tournament.id = Obstacle.tournament_id GROUP BY Tournament.id',
-    next
-  )
-
+  const rows = await executeQuery(SELECT_TOURNAMENTS, next)
   res.json(rows)
 })
 
 tournamentsRouter.post('/', middleware.validateToken, async (req, res, next) => {
   const query = {
-    text: 'INSERT INTO Tournament(account_id, name, created, active) VALUES ($1, $2, $3, $4) RETURNING *',
+    text: INSERT_TOURNAMENT,
     values: [req.account_id, req.body.name, new Date(), true]
+  }
+
+  const rows = await executeQuery(
+    query, next
+  )
+
+  res.json(rows[0])
+})
+
+tournamentsRouter.put('/:id', middleware.validateToken, async (req, res, next) => {
+  const { active } = req.body
+  const tournament_id = req.params.id
+
+  const query = {
+    text: UPDATE_TOURNAMENT,
+    values: [active, tournament_id]
   }
 
   const rows = await executeQuery(
--- db/queries.js
+++ db/queries.js
@@ -11,6 +11,12 @@ const SELECT_PLAYERS =
   'RIGHT JOIN Player ON Player.id = p.player_id ' +
   'GROUP BY Player.id'
 
+const SELECT_TOURNAMENTS =
+  'SELECT Tournament.id, Tournament.account_id, Tournament.name, Tournament.created, Tournament.active, json_agg(Obstacle) AS obstacles ' +
+  'FROM Tournament ' +
+  'LEFT JOIN Obstacle ON Tournament.id = Obstacle.tournament_id ' +
+  'GROUP BY Tournament.id'
+
 const INSERT_PLAYER =
   'INSERT INTO Player(tournament_id, nickname) ' +
   'VALUES ($1, $2) ' +
@@ -25,10 +31,24 @@ const INSERT_OBSTACLERESULT =
   'INSERT INTO ObstacleResult(result_id, obstacle_id) ' +
   'VALUES ($1, $2)'
 
+const INSERT_TOURNAMENT =
+  'INSERT INTO Tournament(account_id, name, created, active) ' +
+  'VALUES ($1, $2, $3, $4) ' +
+  'RETURNING *'
+
+const UPDATE_TOURNAMENT =
+  'UPDATE Tournament ' +
+  'SET active = $1 ' +
+  'WHERE id = $2 ' +
+  'RETURNING *'
+
 module.exports = {
   SELECT_RESULTS,
   SELECT_PLAYERS,
+  SELECT_TOURNAMENTS,
   INSERT_PLAYER,
   INSERT_RESULT,
-  INSERT_OBSTACLERESULT
+  INSERT_OBSTACLERESULT,
+  INSERT_TOURNAMENT,
+  UPDATE_TOURNAMENT
 }
